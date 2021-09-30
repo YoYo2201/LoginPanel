addf6f8db16bc7ac2938a587836a4352410d7804
Manas Agrawal <81013190+YoYo2201@users.noreply.github.com>
2021-09-30 12:51:37 +0530

    Update Config.java

--- app/src/main/java/com/example/loginpanel/Config.java
+++ app/src/main/java/com/example/loginpanel/Config.java
@@ -2,5 +2,5 @@ package com.example.loginpanel;
 
 public class Config {
     public static final String EMAIL = "hoteltablebook@gmail.com";
-    public static final String PASSWORD = "9691455600@CSE";
+    //Password has been removed from here for security purposes...
 }
